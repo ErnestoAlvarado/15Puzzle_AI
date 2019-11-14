745a93aa0774ee3958d9385748cefefa7596f494
ErnestoAlvarado <ernesto.alvarado@ymail.com>
2019-11-14 02:08:56 -0800

    Set up classes to be used for game

--- displayGrid.py
+++ displayGrid.py
@@ -6,4 +6,42 @@ import pygame
 pygame.init()
 
 
+class LoadAssets:
+    """ This class will load in all the assets needed for the game """
+
+    def load_images(self):
+        raise NotImplementedError
+
+class DrawGrid:
+    """ This class will create the playable grid, and update the squares when the player
+        makes a move, or the AI
+    """
+
+    def __init__(self):
+        raise NotImplementedError
+
+    def move_square(self):
+        raise NotImplementedError
+
+    def draw_board(self):
+        raise NotImplementedError
+
+    def reset(self):
+        raise NotImplementedError
+
+
+class Grid:
+    """ This class will create a 2-D array to handle the tiles """
+
+    def __init__(self):
+        raise NotImplementedError
+
+    def move_square(self):
+        raise NotImplementedError
+
+    def search_empty_square(self):
+        raise NotImplementedError
+
+    def shuffle(self):
+        raise NotImplementedError
 
--- game.py
+++ game.py
@@ -1,2 +1,9 @@
 # This file will handle all the logic for the 15 puzzle game
 
+
+class Game:
+    """ This class will handle the game loop """
+
+    def __init__(self):
+        raise NotImplementedError
+
