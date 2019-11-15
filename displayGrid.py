6d9e1d3db6bc85189b23a26cf7d1a9514443cd3e
ErnestoAlvarado <ernesto.alvarado@ymail.com>
2019-11-15 00:37:58 -0800

    Added basic setup to Grid class
    - Grid class now creates a 2-D array using the size variable passed in
    - Once a 2-D array is created, it is filled with numbers between 1 and size*size
    - These numbers are then shuffled randomly

--- displayGrid.py
+++ displayGrid.py
@@ -1,6 +1,7 @@
 # This file will handle all the updates on the grid when the player or AI is playing
 
 import pygame
+import random
 
 # initialize pygame
 pygame.init()
@@ -11,6 +12,7 @@ class LoadAssets:
 
     def load_images(self):
         raise NotImplementedError
+
 
 class DrawGrid:
     """ This class will create the playable grid, and update the squares when the player
@@ -33,15 +35,50 @@ class DrawGrid:
 class Grid:
     """ This class will create a 2-D array to handle the tiles """
 
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, size):
+        # create a 2-D array to hold the tiles
+        self.grid_size = size * size
+        self.grid = [[0]*size for _ in range(size)]
+        self.goal_grid = [[0]*size for _ in range(size)]
 
-    def move_square(self):
+        # create list of tiles based on size of grid
+        self.tiles = [i for i in range(1, self.grid_size)]
+        copy_tiles = self.tiles.copy()
+        copy_tiles.append(0)
+        num = 0
+
+        # fill 2-D array with win condition
+        for i in range(size):
+            for j in range(size):
+                self.goal_grid[i][j] = copy_tiles[num]
+                num += 1
+
+        # randomly shuffle tiles
+        self.shuffle(size)
+
+    def move_square(self, tile_number):
         raise NotImplementedError
 
     def search_empty_square(self):
         raise NotImplementedError
 
-    def shuffle(self):
-        raise NotImplementedError
+    def shuffle(self, size):
+        # randomly shuffle tiles and ensure that goal state is not reached accidentally
+        tile_list = self.tiles.copy()
+        random.shuffle(tile_list)
 
+        # add a 0 to the end of list to indicate empty space
+        tile_list.append(0)
+        c = 0
+
+        # fill 2-D array with tiles that have been shuffled
+        for i in range(size):
+            for j in range(size):
+                self.grid[i][j] = tile_list[c]
+                c += 1
+
+    def check_goal_state(self):
+        return self.grid == self.goal_grid
+
+
+# if __name__ == '__main__':
